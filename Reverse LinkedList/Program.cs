28b8eb27e363f9c478938ef1b898c571c1b749f0
Jaydeep Shil <ED8A@eurofins.com>
2020-10-20 19:25:30 +0530

    Add project files.

---
+++ Reverse LinkedList/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Reverse_LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode head = new ListNode(1);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(3);
+            head.next.next.next = new ListNode(4);
+            ReverseList(head);
+            ReverseList_Recur(head, null);
+        }
+
+        public class ListNode
+        {
+            public int val { get; set; }
+            public ListNode next { get; set; }
+
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        static ListNode ReverseList(ListNode head)
+        {
+            ListNode cur = head;
+            ListNode prev = null;
+            while (cur != null)
+            {
+                ListNode next = cur.next;
+                cur.next = prev;
+                prev = cur;
+                cur = next;
+            }
+            return prev;
+        }
+
+        static ListNode ReverseList_Recur(ListNode head, ListNode prev)
+        {
+            if (head == null) return prev;
+            ListNode next = head.next;
+            head.next = prev;
+            return ReverseList_Recur(next, head);
+        }
+    }
+}
